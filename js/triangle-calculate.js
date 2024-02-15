e37ddfb5daa6914955020c1f7839ee8b6d2e3f23
auishi22 <auishi229@gmail.com>
2024-02-15 23:34:31 +0600

    rec and para done

--- js/triangle-calculate.js
+++ js/triangle-calculate.js
@@ -13,7 +13,44 @@ function triangleCalculate() {
   const area = 0.5 * base * height;
   console.log(area);
 
+  //display area
   const getTriangleArea = document.getElementById("triangle-area");
   getTriangleArea.innerText = area;
-  
+}
+
+//rectangular calculator
+function rectangleCalculate() {
+  const width = getInputValueById("rectangle-width");
+  console.log("Rectangle width: ", width);
+  const lenght = getInputValueById("rectangle-lenght");
+  console.log("Rectangle lenght: ", lenght);
+
+  const area = 0.5 * width * lenght;
+  console.log("Area of rectangle: ", area);
+
+  const displayArea = setInnerTextById("rectangle-area",area);
+
+}
+//parallelogram calculator
+function parallelogramCalculate() {
+  const base = getInputValueById("para-base");
+  console.log("Parallelogram base: ", base);
+  const height = getInputValueById("para-height");
+  console.log("Parallelogram height: ", height);
+
+  const area = base * height;
+  console.log("Area of parallelogram: ", area);
+
+  const displayArea = setInnerTextById("para-area", area);
+}
+
+function getInputValueById(inputFieldId) {
+  const inputField = document.getElementById(inputFieldId);
+  const inputValue = inputField.value;
+  const input = parseFloat(inputValue);
+  return input;
+}
+function setInnerTextById(elementId, area) {
+  const element = document.getElementById(elementId);
+  element.innerText = area;
 }
